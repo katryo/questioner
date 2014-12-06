b13a973deee6667a0786fc5cd7c00bcad9b6658c
Ryo Kato <katoryo55@gmail.com>
2014-12-06 18:12:18 +0900

    ユーザー名が入る

--- public/main.js
+++ public/main.js
@@ -3,6 +3,16 @@ $(function() {
   var $window = $(window);
   var $message_input = $('#message__input');
 
+  var vm = new Vue({ el: 'body', data: {
+    currentUser: '???',
+    messages: [],
+    methods: {
+      appendMessageUpToLimit: function(msg) {
+        console.log(msg);
+      }
+    }
+  }});
+
   $('form#chat').submit(function() {
     socket.emit('chat message', $message_input.val());
     $message_input.val('');
@@ -10,7 +20,9 @@ $(function() {
   });
 
   socket.on('chat message', function(msg) {
-    $('#messages').append($('<li>').text(msg));
+    vm.$data.messages.push({ user: vm.currentUser, content: msg});
+    console.log(vm.$data.messages);
+//    $('#messages').append($('<li>').text(msg));
   });
 
 });
