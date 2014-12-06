73483748bda9da948c7fe585c80df2e65df7a035
Ryo Kato <katoryo55@gmail.com>
2014-12-06 20:10:26 +0900

    timerできた

--- public/main.js
+++ public/main.js
@@ -3,15 +3,45 @@ $(function() {
   var $window = $(window);
   var $message_input = $('#message__input');
 
-  var vm = new Vue({ el: 'body', data: {
-    currentUser: '???',
-    messages: [],
+  var vm = new Vue({
+    el: 'body',
+    data: {
+      currentUser: '???',
+      messages: [],
+      secondsLeft: 0
+    },
     methods: {
       appendMessageUpToLimit: function(msg) {
         console.log(msg);
+      },
+      startTimer: function() {
+        var that = this;
+        window.setInterval(function() {
+          that.secondsLeft = that.secondsLeft - 1;
+        }, 1000);
       }
+    },
+    ready: function() {
+      console.log('aaa');
+      var that = this;
+      $.ajax({
+        type: 'GET',
+        datatype: 'json',
+        url: '/deadline',
+        success: function(data) {
+          var now = Date.now();
+          var deadline = new Date(2014, 11, 6, parseInt(data.hour) + 1, parseInt(data.min));
+          var deadlineMinSec = deadline.getTime();
+          var minSecLeft = deadlineMinSec - now;
+          that.secondsLeft = Math.floor(minSecLeft / 1000);
+          that.startTimer();
+        },
+        error: function(err) {
+          console.log(err);
+        }
+      });
     }
-  }});
+  });
 
   $('form#chat').submit(function() {
     socket.emit('chat message', $message_input.val());
@@ -21,8 +51,6 @@ $(function() {
 
   socket.on('chat message', function(msg) {
     vm.$data.messages.push({ user: vm.currentUser, content: msg});
-    console.log(vm.$data.messages);
-//    $('#messages').append($('<li>').text(msg));
   });
 
 });
--- server.js
+++ server.js
@@ -4,11 +4,21 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/public'));
+if (process.env.REDISTOGO_URL) {
+  var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+  var redis = require("redis").createClient(rtg.port, rtg.hostname);
+  redis.auth(rtg.auth.split(":")[1]);
+} else {
+  var redis = require("redis").createClient();
+}
+
+app.use('/public', express.static(__dirname + '/public'));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 app.get('/', function (req, res) {
-  console.log(req.query);
+  redis.get('question', function(err, reply) {
+    redis.set('question', 'is ...?', redis.print);
+  });
   res.sendFile(__dirname + '/public/index.html');
 });
 
@@ -16,17 +26,40 @@ app.get('/answer', function (req, res) {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-app.get('/set_timer_to_10min', function (req, res) {
-  if(req.query.a === 'kat') {
-  };
-  res.sendFile(__dirname + '/public/index.html');
+app.get('/set_deadline', function (req, res) {
+  if(req.query.admin === 'kat') {
+    var deadline = req.query.deadline;
+    redis.set('deadline', deadline);
+    res.send('deadline set to ' + deadline);
+  } else {
+    res.send('you are not admin!');
+  }
+});
+
+app.get('/deadline', function (req, res) {
+  redis.get('deadline', function(err, reply) {
+    if(err) {
+      res.send({hour: '21', min: '15'});
+    } else {
+      if(reply) {
+        var tenHourStr = reply.charAt(0);
+        var oneHourStr = reply.charAt(1);
+        var hour = tenHourStr + oneHourStr;
+        var tenMinStr = reply.charAt(2);
+        var oneMinStr = reply.charAt(3);
+        var min = tenMinStr + oneMinStr;
+        res.send({hour: hour, min:min});
+      } else {
+        res.send({hour: '21', min: '15'});
+      }
+    }
+  });
 });
 
 io.on('connection', function(socket) {
   socket.on('chat message', function(msg) {
     io.emit('chat message', msg);
   });
-
 });
 
 http.listen(port, function() {
